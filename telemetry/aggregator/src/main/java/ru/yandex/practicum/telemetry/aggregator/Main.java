547d640e83e2cb962221166333d3e0a8ba925f01
Makar-FP <makarka00999@yandex.ru>
2025-11-26 00:56:18 +0300

    v 2.0.0.6

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Main.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/Main.java
@@ -2,10 +2,18 @@ package ru.yandex.practicum.telemetry.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.telemetry.aggregator.service.SensorSnapshotAggregator;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+
+        SensorSnapshotAggregator aggregator = context.getBean(SensorSnapshotAggregator.class);
+        aggregator.start();
     }
 }
