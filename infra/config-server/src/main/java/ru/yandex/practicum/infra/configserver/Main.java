364fbd9c39f67d29a2fcc61fc507bf8fd8beddac
Makar-FP <makarka00999@yandex.ru>
2025-11-30 18:09:31 +0300

    config v1.0.0.0

--- infra/config-server/src/main/java/ru/yandex/practicum/infra/configserver/Main.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/infra/configserver/Main.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
 }
